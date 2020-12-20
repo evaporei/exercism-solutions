eea0186bf68ea3886c496696229a484dae7208ca
Otavio Pace <otaviopp8@gmail.com>
2020-12-20 12:42:06 -0300

    rust: wordy pass test 1

--- rust/wordy/src/lib.rs
+++ rust/wordy/src/lib.rs
@@ -1,8 +1,6 @@
 pub struct WordProblem;
 
 pub fn answer(command: &str) -> Option<i32> {
-    unimplemented!(
-        "Return the result of the command '{}' or None, if the command is invalid.",
-        command
-    );
+    let result = command.replace("What is ", "").replace("?", "");
+    result.parse::<i32>().ok()
 }
